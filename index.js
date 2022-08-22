b11d96d32bd0451b9ba265841dbcacc1128a21cc
KauanAmorim <kauanamorim88@gmail.com>
2022-08-22 01:03:50 -0300

    some adjusts

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import 'dotenv/config';
-import app from './src/app';
+import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 });
-
