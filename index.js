e796fe7bdfce96238547c3770e6615f15b254073
KauanAmorim <kauanamorim88@gmail.com>
2022-08-22 00:59:11 -0300

    created express app

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import app from './src/app';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
+
